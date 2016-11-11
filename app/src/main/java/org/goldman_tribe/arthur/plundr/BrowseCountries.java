5c821417738c85105a0d410dc5b703d4de3b195f
Arthur Goldman <arthur@goldman-tribe.org>
2016-11-11 14:16:15 +0000

    Fixed the git mess?

---
+++ app/src/main/java/org/goldman_tribe/arthur/plundr/BrowseCountries.java
@@ -0,0 +1,13 @@
+package org.goldman_tribe.arthur.plundr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrowseCountries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_countries);
+    }
+}
