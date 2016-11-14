cb4522ea655f41326a9a743a5fce35fe3294bf45
Arthur Goldman <arthur@goldman-tribe.org>
2016-11-14 14:49:45 +0000

    fixed the layout!

--- app/src/main/java/org/goldman_tribe/arthur/plundr/BrowseCountries.java
+++ app/src/main/java/org/goldman_tribe/arthur/plundr/BrowseCountries.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Objects;
@@ -17,34 +16,18 @@ public class BrowseCountries extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_countries);
 
-        RelativeLayout.LayoutParams params =
-                new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.MATCH_PARENT,
-                        RelativeLayout.LayoutParams.WRAP_CONTENT);
-
         Intent intent = getIntent();
         String countryName = intent.getStringExtra(MainActivity.COUNTRY_NAME);
         long countryDef$$$ = intent.getLongExtra(MainActivity.COUNTRY_DEF_$$$, 0L);
         String countryDescription = intent.getStringExtra(MainActivity.COUNTRY_DESCRIPTION);
 
-        TextView Title = new TextView(this);
+        TextView Title = (TextView)findViewById(R.id.country_name);
         Title.setText(countryName);
-        Title.setTextSize(40);
-        Title.setLayoutParams(params);
 
-        TextView Description = new TextView(this);
+        TextView Description = (TextView)findViewById(R.id.country_description);
         Description.setText(countryDescription);
-        Description.setTextSize(25);
-        Description.setLayoutParams(params);
 
-        TextView Budget = new TextView(this);
+        TextView Budget = (TextView)findViewById(R.id.country_budget);
         Budget.setText("$" + Objects.toString(countryDef$$$, null) + " in yearly defense spendings");
-        Budget.setTextSize(25);
-        Budget.setLayoutParams(params);
-
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_browse_countries);
-        layout.addView(Title);
-        layout.addView(Budget);
-        layout.addView(Description);
     }
 }
